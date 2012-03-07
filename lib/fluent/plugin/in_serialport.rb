eae1a221ff6efff5aa14962e25ab9a31d25fa05b
MATSUMOTO Katsuyoshi <matsumoto.katsuyoshi+github@gmail.com>
2012-03-07 22:52:07 +0900

    fixed bug...
    &method argment needs symbol.

--- lib/fluent/plugin/in_serialport.rb
+++ lib/fluent/plugin/in_serialport.rb
@@ -1,17 +1,16 @@
 module Fluent
 class SerialPortInput < Input
   Plugin.register_input('serial_input', self)
-  config_param :data, :string, :default => ""
-  config_param :delimiter, :string, :default => ','
-  config_param :eol, :string, :default => $/
   config_param :com_port, :string
   config_param :baud_rate, :integer
   config_param :tag, :string, :default => "serial"
+  config_param :data, :string, :default => ""
+  config_param :delimiter, :string, :default => ","
+  config_param :eol, :string, :default => $/
 
   def initialize
     require 'serialport'
     super
-    @serial = SerialPort.new(@com_port, @baud_rate, 8, 1, SerialPort::NONE)
   end
 
   def configure(conf)
@@ -19,7 +18,8 @@ class SerialPortInput < Input
   end
 
   def start
-    @thread = Thread.new(&method(run))
+    @serial = SerialPort.new(@com_port, @baud_rate, 8, 1, SerialPort::NONE)
+    @thread = Thread.new(&method(:run))
   end
 
   def shutdown
@@ -33,8 +33,8 @@ class SerialPortInput < Input
         begin
           data = {:default => @serial.readline(@eol)}
           Engine.emit("#{@tag}.#{device}", Engine.now, data)
-        rescue => e
-          STDERR.puts caller(), e
+        rescue
+          STDERR.puts caller()
           break
         end
       end
