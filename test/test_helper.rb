5c6374ffbb8c2d64fb70936b6b5bc602d42312f0
Kenji Okimoto <okimoto@clear-code.com>
2017-04-27 13:12:23 +0900

    Use bundler/setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,14 +1,7 @@
-require 'bundler'
+require 'bundler/setup'
 require 'simplecov'
 SimpleCov.start
 
-begin
-  Bundler.setup(:default, :development)
-rescue Bundler::BundlerError => e
-  $stderr.puts e.message
-  $stderr.puts "Run `bundle install` to install missing gems"
-  exit e.status_code
-end
 require 'test/unit'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
