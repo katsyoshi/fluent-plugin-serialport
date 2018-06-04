23710c9b8fcf7cefbcdf952668a76dd75b5da4b7
Kenji Okimoto <okimoto@clear-code.com>
2018-06-04 12:23:16 +0900

    Use Fluent::Test::Helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,9 @@ require 'test/unit/rr'
 $LOAD_PATH.unshift(File.join(__dir__, '..', 'lib'))
 $LOAD_PATH.unshift(__dir__)
 require 'fluent/test'
+require 'fluent/test/helpers'
 require 'fluent/test/driver/input'
 
 require 'fluent/plugin/in_serialport'
+
+Test::Unit::TestCase.include(Fluent::Test::Helpers)
